f1764ac32a713989e115b3a4b5b2b42948db46b0
Guilherme Souza <grsouza@pm.me>
2021-08-26 15:25:32 -0300

    Init svelte project

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    singleQuote: true,
+    printWidth: 80,
+    semi: true,
+    svelteSortOrder: 'options-styles-scripts-markup',
+    svelteStrictMode: false,
+    svelteBracketNewLine: true,
+    svelteIndentScriptAndStyle: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+  };
+  
