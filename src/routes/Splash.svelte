93e236f9f093098496f4b6afb78543b8c4ff5dee
Guilherme Souza <grsouza@pm.me>
2021-09-13 11:07:21 -0300

    Small screen improvements

--- src/routes/Splash.svelte
+++ src/routes/Splash.svelte
@@ -11,13 +11,13 @@
 
 <div class="flex flex-grow flex-col justify-between bg-indigo-600">
   <header>
-    <div class="flex flex-row pt-20 px-20 items-center justify-between">
+    <div class="flex flex-row pt-20 md:px-20 px-8 items-center justify-between">
       <FullLogo />
       <Button text="Launch App" onClick={launchApp} mini class="uppercase" />
     </div>
   </header>
   <main>
-    <div class="flex flex-col px-20">
+    <div class="flex flex-col md:px-20 px-8 py-20">
       <div class="grid grid-cols-2">
         <div>
           <h2>Self-Sovereign Storage</h2>
@@ -35,13 +35,13 @@
             class="uppercase mt-3"
           />
         </div>
-        
+
         <div />
       </div>
     </div>
   </main>
   <footer>
-    <div class="flex flex-row flex-grow py-3 px-20 bg-blue-900">
+    <div class="flex flex-row flex-grow py-3 md:px-20 px-8 bg-blue-900">
       <p>Kepler &copy; 2021</p>
     </div>
   </footer>
